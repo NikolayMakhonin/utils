2c69c6120bb91e8601a38e2d691211e6a1f285f1
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-06-21 12:15:14 +0400

    fix: Priority tests, deleted old tests

--- src/sync/priority/Priority.test.ts
+++ src/sync/priority/Priority.test.ts
@@ -4,65 +4,6 @@ import {createTestVariants} from '@flemist/test-variants'
 describe('priority > Priority', function () {
   this.timeout(120000)
 
-  function test(o1: Priority, o2: Priority, checkResult: -1 | 0 | 1) {
-    let result = priorityCompare(o1, o2)
-    assert.strictEqual(result, checkResult)
-    result = priorityCompare(o2, o1)
-    if (result !== 0) {
-      result = -result
-    }
-    assert.strictEqual(result, checkResult)
-  }
-
-  it('base', function () {
-    test(void 0, void 0, 0)
-    test(null, null, 0)
-    test(void 0, null, 0)
-
-    test(null, priorityCreate(0), 0)
-    test(null, priorityCreate(-1), 1)
-    test(null, priorityCreate(1), -1)
-
-    test(void 0, priorityCreate(0), 0)
-    test(void 0, priorityCreate(-1), 1)
-    test(void 0, priorityCreate(1), -1)
-
-    test(priorityCreate(0), priorityCreate(0), 0)
-    test(priorityCreate(0), priorityCreate(1), -1)
-    test(priorityCreate(0), priorityCreate(-1), 1)
-
-    test(priorityCreate(1), priorityCreate(1), 0)
-    test(priorityCreate(-1), priorityCreate(-1), 0)
-    test(priorityCreate(1), priorityCreate(-1), 1)
-
-    test(priorityCreate(0), priorityCreate(0, priorityCreate(0)), 0)
-    test(priorityCreate(0), priorityCreate(0, priorityCreate(-1)), 1)
-    test(priorityCreate(0), priorityCreate(0, priorityCreate(1)), -1)
-    test(priorityCreate(0), priorityCreate(1, priorityCreate(0)), -1)
-    test(priorityCreate(0), priorityCreate(-1, priorityCreate(0)), 1)
-
-    test(priorityCreate(-1), priorityCreate(0, priorityCreate(0)), -1)
-    test(priorityCreate(-1), priorityCreate(0, priorityCreate(-1)), 0)
-    test(priorityCreate(-1), priorityCreate(0, priorityCreate(1)), -1)
-
-    test(priorityCreate(1), priorityCreate(0, priorityCreate(0)), 1)
-    test(priorityCreate(1), priorityCreate(0, priorityCreate(-1)), 1)
-    test(priorityCreate(1), priorityCreate(0, priorityCreate(1)), 0)
-
-
-    test(priorityCreate(0, priorityCreate(0)), priorityCreate(0, priorityCreate(0)), 0)
-    test(priorityCreate(0, priorityCreate(0)), priorityCreate(0, priorityCreate(-1)), 1)
-    test(priorityCreate(0, priorityCreate(0)), priorityCreate(0, priorityCreate(1)), -1)
-
-    test(priorityCreate(-1, priorityCreate(0)), priorityCreate(0, priorityCreate(0)), -1)
-    test(priorityCreate(-1, priorityCreate(0)), priorityCreate(0, priorityCreate(-1)), 1)
-    test(priorityCreate(-1, priorityCreate(0)), priorityCreate(0, priorityCreate(1)), -1)
-
-    test(priorityCreate(1, priorityCreate(0)), priorityCreate(0, priorityCreate(0)), 1)
-    test(priorityCreate(1, priorityCreate(0)), priorityCreate(0, priorityCreate(-1)), 1)
-    test(priorityCreate(1, priorityCreate(0)), priorityCreate(0, priorityCreate(1)), -1)
-  })
-  
   function orderToString(order: number) {
     return order == null
       ? '50'
