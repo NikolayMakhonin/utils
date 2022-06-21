f3bd1a8af646d663288b2b94262e3753cf419d00
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2022-06-21 12:13:53 +0400

    fix: Priority tests

--- src/sync/priority/Priority.ts
+++ src/sync/priority/Priority.ts
@@ -27,35 +27,18 @@ export function priorityCreate(order: number, parent?: Priority) {
 }
 
 export function priorityCompare(o1: Priority, o2: Priority): number {
-  if (o1 == null) {
-    if (o2 == null) {
-      return 0
-    }
-    return o2.order <= 0 ? 1 : -1
-  }
-  if (o2 == null) {
-    return o1.order <= 0 ? -1 : 1
-  }
-
-  const b1 = o1.branch
-  const b2 = o2.branch
-  const len1 = b1.length
-  const len2 = b2.length
-  const len = len1 < len2 ? len1 : len2
+  const b1 = o1 && o1.branch
+  const b2 = o2 && o2.branch
+  const len1 = b1 ? b1.length : 0
+  const len2 = b2 ? b2.length : 0
+  const len = len1 > len2 ? len1 : len2
 
   for (let i = 0; i < len; i++) {
-    const order1 = b1[len1 - 1 - i]
-    const order2 = b2[len2 - 1 - i]
+    const order1 = i >= len1 ? 0 : b1[len1 - 1 - i]
+    const order2 = i >= len2 ? 0 : b2[len2 - 1 - i]
     if (order1 !== order2) {
       return order1 > order2 ? 1 : -1
     }
-  }
-
-  if (len1 < len2) {
-    return b2[len2 - len - 1] <= 0 ? 1 : -1
-  }
-  if (len1 > len2) {
-    return b1[len1 - len - 1] <= 0 ? -1 : 1
   }
 
   return 0
